eb4c5b43ea7c6ceccd7edfec60c9cc50c1fae50d
Hiral <132593143+hiral74@users.noreply.github.com>
2023-05-07 15:11:46 +0530

    Add files via upload

---
+++ constants/styles.js
@@ -0,0 +1,18 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#e4d9fd', //last even days box
+    primary100: '#c6affc', //add expenses box
+    primary200: '#a281f0',
+    primary400: '#5721d4',
+    primary500: '#638B9E', //header
+    primary700: '#9DD4C9', //green bg
+    primary800: '#638B9E', // add expenses
+    accent500: '#e5acb6', // icon colour
+    error50: '#638B9E',
+    error500: '#9b095c',//dusbin colour
+    gray500: '#EFF0ED',
+    gray700: '#221c30',
+  },
+};
+
+//#e5acb6
